8ac18e7d9e079ca3e1890e5f7ba72fa75f47d641
vfcruz <vfcruz@campus.fct.unl.pt>
2022-04-24 12:36:00 +0100

    JavaFiles

--- src/tp1/server/resources/JavaFiles.java
+++ src/tp1/server/resources/JavaFiles.java
@@ -2,7 +2,6 @@ package tp1.server.resources;
 
 import java.io.File;
 import java.io.FileNotFoundException;
-import java.io.FileOutputStream;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.nio.charset.StandardCharsets;
@@ -10,34 +9,38 @@ import java.util.Scanner;
 
 import tp1.api.service.util.Files;
 import tp1.api.service.util.Result;
-import java.nio.file.Paths;
-import java.util.logging.Logger;
 
 public class JavaFiles implements Files{
 
     public JavaFiles() {}
 
-    private static Logger Log = Logger.getLogger(JavaFiles.class.getName());
-
     @Override
     public Result<Void> writeFile(String fileId, byte[] data, String token) {
         // Check if file is valid
-         // Check if file data is valid
-         if(fileId == null || data == null) {
-          Log.info("File invalid.");
-          return Result.error(Result.ErrorCode.BAD_REQUEST );
-      }
+        if(fileId == null || data == null) {
+            return Result.error(Result.ErrorCode.BAD_REQUEST);
+        }
 
-      try {
-          File outputFile = new File(fileId);
-          try (FileOutputStream outputStream = new FileOutputStream(outputFile)) {
-              outputStream.write(data);
+        try {
+            File myObj = new File(fileId);
+            myObj.createNewFile();
+          } catch (IOException e) {
+            System.out.println("An error occurred.");
+            e.printStackTrace();
           }
 
-      } catch (IOException e) {
-          e.printStackTrace();
-      }
-      return Result.ok();
+        try {
+            FileWriter myWriter = new FileWriter(fileId);
+            myWriter.write(new String(data, StandardCharsets.UTF_8));
+            myWriter.close();
+            System.out.println("Successfully wrote to the file.");
+
+            return Result.ok();
+          } catch (IOException e) {
+            System.out.println("An error occurred.");
+            e.printStackTrace();
+            return Result.error(Result.ErrorCode.FORBIDDEN);
+          }
 
 
     }
@@ -45,41 +48,41 @@ public class JavaFiles implements Files{
     @Override
     public Result<Void> deleteFile(String fileId, String token) {
         // Check if file is valid
-        // Check if file data is valid
-        if(fileId == null ){
-          Log.info("FileId invalid.");
-          return Result.error(Result.ErrorCode.BAD_REQUEST);
-      }
+        if(fileId == null) {
+            return Result.error(Result.ErrorCode.BAD_REQUEST);
+        }
 
-      try {
-          java.nio.file.Files.delete(Paths.get(fileId));
-      }catch (IOException e) {
-          Log.info("File does not exist.");
-          return Result.error(Result.ErrorCode.NOT_FOUND );
-      }
+        File myObj = new File(fileId);
+        myObj.delete();
 
-      return Result.ok();
+        return Result.ok();
     }
 
     @Override
     public Result<byte[]> getFile(String fileId, String token) {
-      Log.info("getFile : " + fileId);
+        // Check if file is valid
+        if(fileId == null) {
+            return Result.error(Result.ErrorCode.BAD_REQUEST);
+        }
 
-      // Check if file data is valid
-      if(fileId == null ){
-          Log.info("FileId invalid.");
-          return Result.error(Result.ErrorCode.BAD_REQUEST);
-      }
+        String readFile = "";
+        
+        try {
+            File myObj = new File(fileId);
+            Scanner myReader = new Scanner(myObj);
+            while (myReader.hasNextLine()) {
+              String data = myReader.nextLine();
+              readFile.concat(data);
+            }
+            myReader.close();
 
-      byte[] file;
-      try {
-          file = java.nio.file.Files.readAllBytes(Paths.get(fileId));
-          return Result.ok(file);
+            return Result.ok(readFile.getBytes());
+          } catch (FileNotFoundException e) {
+            System.out.println("An error occurred.");
+            e.printStackTrace();
+            return Result.error(Result.ErrorCode.NOT_FOUND);
+          }
 
-      } catch (IOException e) {
-          Log.info("File does not exist.");
-          return Result.error(Result.ErrorCode.NOT_FOUND);
-      }
 
     }
 }
